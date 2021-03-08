12a016a701ba7854a69b830c72ff90fd7e1dedfd
Chirag <cdhawan7@gmail.com>
2021-03-08 23:17:44 +0530

    Added flight simulation demo.

---
+++ flight simulation/cyclone.h
@@ -0,0 +1,13 @@
+/* import headers for all.*/
+#include "precision.h"
+#include "core.h"
+#include "random.h"
+// #include "particle.h"
+// #include "pcontacts.h"
+// #include "plinks.h"
+// #include "pfgen.h"
+// #include "pworld.h"
+#include "world.h"
+#include "fgen.h"
+#include "collide_fine.h"
+#include "contacts.h"
