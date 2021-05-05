7175f39d772103a4281732b85728c85126d53081
agrim334 <agrim334@gmail.com>
2021-05-05 19:43:32 +0530

    small upd

--- sailboat/sailboat.cpp
+++ sailboat/sailboat.cpp
@@ -1,15 +1,3 @@
-/*
- * The sailboat demo.
- *
- * Part of the Cyclone physics system.
- *
- * Copyright (c) Icosagon 2003. All Rights Reserved.
- *
- * This software is distributed under licence. Use of this software
- * implies agreement with all terms and conditions of the accompanying
- * software licence.
- */
-
 #include "cyclone.h"
 #include "opengl_headers.h"
 #include "app.h"
--- shooting/cyclone.h
+++ shooting/cyclone.h
@@ -3,8 +3,4 @@
 #include "core.h"
 #include "random.h"
 #include "particle.h"
-#include "pcontacts.h"
-#include "plinks.h"
-#include "pfgen.h"
-#include "pworld.h"
 
